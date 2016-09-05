2872019e61c388225b9c8328c08dc3790e575d9a
sudha s <sudha s>
2016-09-05 10:52:39 -0400

    Version2
    Add and Delete works

--- script.js
+++ script.js
@@ -4,19 +4,50 @@ window.onload = function(){
 	var email = document.getElementById('email');
 	var resetBtn = document.getElementById('reset');
 	var addBtn = document.getElementById('add');
+	var deleteBtn = document.getElementById('delete');
 	var addressBook = document.querySelector('.address-book');
 	var addressForm = document.querySelector('.address-form');
-
-	resetBtn.addEventListener("click", function(){
-		name.value = "";
-		phone.value = "";
-	    email.value = "";
+	var addressBookData=[];
 		
-	});
+	showAddressBook();
+	
+	function showAddressBook(){
+		if(localStorage['addbook'] === undefined){
+			localStorage['addbook'] = '';
+		} else {
+			addressBookData = JSON.parse(localStorage['addbook']);
+			for(var n in addressBookData){
+				var tr = document.createElement("tr");
+				createRow(tr, n, addressBookData);
+			}
+		}
+	}
 	
 	addBtn.addEventListener("click", addToBook);
+	deleteBtn.addEventListener("click", deleteFromBook);
+	resetBtn.addEventListener("click", resetFields)
 	
-	var addressBookData=[];
+	function addToBook(){
+		if(name.value!='' && phone.value!='' && email.value!=''){
+			var obj = new jsonStructure(name.value,phone.value,email.value)
+		    addressBookData.push(obj);
+		    localStorage['addbook'] = JSON.stringify(addressBookData);
+            addNewlyAddedAddress();
+		}
+	}
+	
+	function deleteFromBook(){
+		var table = document.getElementById('address-book');
+		var chekbox = document.getElementsByName('checkboxAddressList');
+		addressBookData = JSON.parse(localStorage['addbook']);
+		for(var n in chekbox){
+			if(null != chekbox[n] && true == chekbox[n].checked) {
+				table.deleteRow(n);
+				addressBookData.splice(n,1);
+			}	
+		}
+		localStorage['addbook'] = JSON.stringify(addressBookData);
+	}
 	
 	function jsonStructure(name,phone,email){
 		this.name = name;
@@ -24,18 +55,21 @@ window.onload = function(){
 		this.email = email;
 	}
 	
-	function addToBook(){
-		if(name.value!='' && phone.value!='' && email.value!=''){
-			var obj = new jsonStructure(name.value,phone.value,email.value)
-		    addressBookData.push(obj);
-		    localStorage['addbook'] = JSON.stringify(addressBookData);
-            refreshAddressBook();
+	function addNewlyAddedAddress(){
+		if(localStorage['addbook'] === undefined){
+			localStorage['addbook'] = '';
+		} else {
+			addressBookData = JSON.parse(localStorage['addbook']);
+			var n = addressBookData.length - 1;
+			var tr=document.createElement("tr");
+			createRow(tr, n, addressBookData);
 		}
 	}
 	
-function createRow(tr, n, addressBookData){
+	function createRow(tr, n, addressBookData){
 		var td1 = document.createElement("input");
-        td1.type = 'checkbox';
+		td1.type = 'checkbox';
+		td1.name = 'checkboxAddressList'
 		var td2=document.createElement("td");
 		var td3=document.createElement("td");
 		var td4=document.createElement("td");
@@ -52,28 +86,9 @@ function createRow(tr, n, addressBookData){
 			
 	}
 	
-	function showAddressBook(){
-		if(localStorage['addbook'] === undefined){
-			localStorage['addbook'] = '';
-		} else {
-			addressBookData = JSON.parse(localStorage['addbook']);
-			for(var n in addressBookData){
-				var tr=document.createElement("tr");
-				createRow(tr, n, addressBookData);
-			}
-		}
+	function resetFields(){
+		name.value = "";
+		phone.value = "";
+	    email.value = "";
 	}
-	
-	function refreshAddressBook(){
-		if(localStorage['addbook'] === undefined){
-			localStorage['addbook'] = '';
-		} else {
-			addressBookData = JSON.parse(localStorage['addbook']);
-			var n = addressBookData.length - 1;
-			var tr=document.createElement("tr");
-			createRow(tr, n, addressBookData);
-		}
-	}
-	
-	showAddressBook();
 }
